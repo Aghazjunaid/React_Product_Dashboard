04a6fc2290479d0f14918b4c2db436c0cd5ebd71
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-05 19:48:32 +0530

    register logic

--- src/components/register.js
+++ src/components/register.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {useHistory} from 'react-router-dom';
 
 function Register() {
-    
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [phone, setPhone] = useState('')
+    const history = useHistory()
+
+    async function register(e){
+        e.preventDefault();
+
+        let userData = {name, email, phone, password}
+        console.log(userData)
+        let result = await fetch("http://localhost:8000/register",{
+            method: 'POST',
+            headers: {
+                "Content-Type":"application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+        result = await result.json();
+        console.log("result",result)
+        localStorage.setItem("userData", JSON.stringify(result));
+        history.push("/add")
+    }
 
   return (
     <div>
@@ -11,22 +35,22 @@ function Register() {
       <Container>
         <Row style={{ marginTop: "60px" }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={register}>
               <Form.Group controlId="formBasicName">
                 <Form.Label>Full Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Full Name" />
+                <Form.Control type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Full Name" />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter Email Address" />
+                <Form.Control type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email Address" />
               </Form.Group>
               <Form.Group controlId="formBasicMobile">
                 <Form.Label>Mobile Number</Form.Label>
-                <Form.Control placeholder="Enter Mobile Number" />
+                <Form.Control value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder="Enter Mobile Number" />
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" />
+                <Form.Control value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Enter Password" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Register
