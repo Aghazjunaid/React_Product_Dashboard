0184e31f4e37317a251301ccf084f51d831e2c09
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-05 23:20:32 +0530

    product listing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Login from './components/login';
-import Register from './components/register';
-import AddProduct from './components/addProduct';
-import UpdateProduct from './components/updateProduct';
-import Protected from './components/Protected';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/login";
+import Register from "./components/register";
+import AddProduct from "./components/addProduct";
+import UpdateProduct from "./components/updateProduct";
+import Protected from "./components/Protected";
+import ProductList from "./components/productList";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-        <Route path="/add" >
-          <Protected component={AddProduct} />
+          <Route path="/" exact>
+            <Protected component={ProductList} />
           </Route>
-        <Route path="/update">
-        <Protected component={UpdateProduct} />
-        </Route>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+          <Route path="/add">
+            <Protected component={AddProduct} />
+          </Route>
+          <Route path="/update">
+            <Protected component={UpdateProduct} />
+          </Route>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
       </Router>
     </div>
