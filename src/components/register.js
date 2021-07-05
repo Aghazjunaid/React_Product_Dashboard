c147203b4238b736320e169b371541e2fb21b8a3
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-05 17:10:11 +0530

    register

--- src/components/register.js
+++ src/components/register.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import Header from './header'
+import React from "react";
+import Header from "./header";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 function Register() {
-    return (
-        <div>
-                  <Header/>
+    
 
-                        <h1>Register</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Row style={{ marginTop: "60px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Full Name" />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email Address" />
+              </Form.Group>
+              <Form.Group controlId="formBasicMobile">
+                <Form.Label>Mobile Number</Form.Label>
+                <Form.Control placeholder="Enter Mobile Number" />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
